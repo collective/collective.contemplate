72966b516e83e05965edbd49f14177d35cb0020f
Ross Patterson <me@rpatterson.net>
2009-04-19 00:24:41 +0000

    Add ZCML to the tests

--- collective/contemplate/tests.py
+++ collective/contemplate/tests.py
@@ -3,6 +3,9 @@ from zope.testing import doctest
 
 from Testing import ZopeTestCase
 from Products.PloneTestCase import ptc
+from Products.Five import zcml
+
+from collective import contemplate
 
 ptc.setupPloneSite()
 
@@ -10,10 +13,14 @@ optionflags = (doctest.NORMALIZE_WHITESPACE|
                doctest.ELLIPSIS|
                doctest.REPORT_NDIFF)
 
+def setUp(test):
+    zcml.load_config(package=contemplate, file='configure.zcml')
+
 def test_suite():
     suite = ZopeTestCase.FunctionalDocFileSuite(
         'README.txt',
         optionflags=optionflags,
+        setUp=setUp,
         test_class=ptc.FunctionalTestCase)
     return suite
 
