688a4ef86088b28a46d071535537e63bb80a66b8
Ross Patterson <me@rpatterson.net>
2009-04-20 04:16:37 +0000

    Make creators change recursive and add some efficiencies

--- collective/contemplate/form.py
+++ collective/contemplate/form.py
@@ -49,9 +49,43 @@ class TemplateAddForm(object):
                 return
             container = Acquisition.aq_parent(container)
 
+    def fixOwnerRole(self, object, user_id):
+        # Get rid of all other owners
+        owners = object.users_with_local_role('Owner')
+        for o in owners:
+            roles = list(object.get_local_roles_for_userid(o))
+            roles.remove('Owner')
+            if roles:
+                object.manage_setLocalRoles(o, roles)
+            else:
+                object.manage_delLocalRoles([o])
+        # Fix for 1750
+        roles = list(object.get_local_roles_for_userid(user_id))
+        roles.append('Owner')
+        object.manage_setLocalRoles(user_id, roles)
+        object.setCreators((user_id,))
+
     def changeOwnership(self, object):
         membership = object.portal_membership
-        user_id = membership.getAuthenticatedMember().getId()
-        object.plone_utils.changeOwnershipOf(
-            object, user_id, recursive=1)
-        object.setCreators((user_id,))
+        acl_users = self.context.acl_users
+        userid = membership.getAuthenticatedMember().getId()
+        user = acl_users.getUserById(userid)
+        if user is None:
+            # The user could be in the top level acl_users folder in
+            # the Zope root, in which case this should find him:
+            user = membership.getMemberById(userid)
+            if user is None:
+                raise KeyError(
+                    'Only retrievable users in this site can be made '
+                    'owners.')
+
+        object.changeOwnership(user, 1)
+        self.fixOwnerRole(object, userid)
+
+        _path = self.context.portal_url.getRelativeContentURL(object)
+        for brain in self.context.portal_catalog(
+            path={'query':_path,'level':1}):
+            obj = brain.getObject()
+            self.fixOwnerRole(obj, userid)
+            obj.reindexObject(
+                self.context._cmf_security_indexes+('Creator',))
