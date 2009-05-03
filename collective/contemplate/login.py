b24b9901a22f4f9ef017cbedc9080618b878cf8a
Ross Patterson <me@rpatterson.net>
2009-05-03 18:21:37 +0000

    Reproduce the template copy logic for memberarea creation so that members can't add folders to the Members folder but ownership cleanup still reflects the right user.

--- collective/contemplate/login.py
+++ collective/contemplate/login.py
@@ -1,16 +1,40 @@
 from zope import component
+from zope import event
+
+from AccessControl import SecurityManagement
 
 from Products.PluggableAuthService.interfaces import events
 from Products.PlonePAS import utils 
 
+from collective.contemplate import interfaces
+from collective.contemplate import owner
+
 @component.adapter(events.IUserLoggedInEvent)
-def createMemberArea(event):
-    """Use invokeFactory so that the template will be used"""
-    if not hasattr(event.principal, 'Members'):
+def createMemberArea(logged_in):
+    """Use the template if available"""
+    if not hasattr(logged_in.principal, 'Members'):
         return
-    safe_member_id = utils.cleanId(event.principal.getId())
-    if event.principal.Members.hasObject(safe_member_id):
+    safe_member_id = utils.cleanId(logged_in.principal.getId())
+    container = logged_in.principal.Members
+    if container.hasObject(safe_member_id):
         return
-    event.principal.Members.invokeFactory(
-        type_name=event.principal.portal_membership.memberarea_type,
-        id=safe_member_id)
+
+    info = logged_in.principal.portal_types.getTypeInfo(
+        logged_in.principal.portal_membership.memberarea_type)
+    template = info.getTemplate(container)
+    if template is None:
+        return
+    orig, source = template
+
+    sm = SecurityManagement.getSecurityManager()
+    SecurityManagement.newSecurityManager(
+        None,
+        logged_in.principal.portal_url.getPortalObject().getOwner())
+    result, = container.manage_pasteObjects(
+        source.manage_copyObjects([orig.getId()]))
+    container.manage_renameObject(result['new_id'], safe_member_id)
+    SecurityManagement.setSecurityManager(sm)
+
+    added = container[safe_member_id]
+    owner.changeOwnershipOf(added)
+    event.notify(interfaces.TemplateCopiedEvent(added, orig))
