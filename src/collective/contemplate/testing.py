2885f4227e34890817be2a5f0ea5037747866ac2
Ross Patterson <me@rpatterson.net>
2010-02-08 08:04:55 +0000

    Use new conveniences from collective.testcaselayer

--- src/collective/contemplate/testing.py
+++ src/collective/contemplate/testing.py
@@ -1,6 +1,5 @@
-from Products.Five import zcml
-
 from collective.testcaselayer import ptc as tcl_ptc
+from collective.testcaselayer import common
 
 from collective import contemplate
 
@@ -8,16 +7,10 @@ class Layer(tcl_ptc.BasePTCLayer):
     """Install collective.contemplate"""
 
     def afterSetUp(self):
-        # Don't ignore exceptions
-        error_props = self.portal.error_log.getProperties()
-        error_props['ignored_exceptions'] = ()
-        error_props = self.portal.error_log.setProperties(
-            **error_props)
-
-        zcml.load_config(package=contemplate, file='configure.zcml')
+        self.loadZCML('configure.zcml', package=contemplate)
         self.addProfile('collective.contemplate:default')
 
-layer = Layer([tcl_ptc.ptc_layer])
+layer = Layer([common.common_layer])
 
 class BatchLayer(tcl_ptc.BasePTCLayer):
     """Add some content for batch editing"""
